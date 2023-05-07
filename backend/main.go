b2ff37675bdba685919c83923e0c555f526ccfe8
Arron2411616261 <15018135093@163.com>
2023-05-07 12:49:18 +0800

    解决前后端分离跨域问题

--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // 前后端分离项目需要注意跨域问题，因此需要添加请求头
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
