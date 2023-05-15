071489574fa5df880101ac99685ab6c7e17f9632
Arron2411616261 <15018135093@163.com>
2023-05-15 23:31:56 +0800

    监听优化

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,33 +1,16 @@
+// 引入初始化样式文件
 import '@/styles/common.scss'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-import { useIntersectionObserver } from '@vueuse/core'
+
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
-
-app.directive('img-lazy', {
-  mounted(el, binding) {
-    // el: 指令绑定的那个元素 img
-    // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
-    console.log(el, binding.value)
-    useIntersectionObserver(
-      el,
-      ([{ isIntersecting }]) => {
-        console.log(isIntersecting)
-        if (isIntersecting) {
-          // 进入视口区域
-          el.src = binding.value
-        }
-      },
-    )
-  }
-})
