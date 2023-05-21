6871d6de9439ee4a80e4f42403527ac033be8cc9
winson02 <13712052798@163.com>
2023-05-21 10:14:21 +0800

    更新了User结构体的内容

--- backend/model/user.go
+++ backend/model/user.go
@@ -6,6 +6,7 @@ type User struct {
 	gorm.Model
 	Name      string `gorm:"type:varchar(20);not null"`
 	Telephone string `gorm:"type:varchar(20);not null;unique"`
+	Account   string `gorm:"type:varchar(20);not null;unique"`
 	Password  string `gorm:"size:255"`
 }
 
