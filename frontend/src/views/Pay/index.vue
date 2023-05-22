9305844d31d76929645a5db1a541681fbfbab9cf
Arron2411616261 <15018135093@163.com>
2023-05-22 21:04:35 +0800

    支付-获取基础支付信息

--- frontend/src/views/Pay/index.vue
+++ frontend/src/views/Pay/index.vue
@@ -1,5 +1,15 @@
 <script setup>
-const payInfo = {}
+import { getOrderAPI } from '@/apis/pay';
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const payInfo = ref({})
+const getPayInfo = async () => {
+  const res = await getOrderAPI(route.query.id)
+  payInfo.value = res.result
+}
+onMounted(() => getPayInfo())
 </script>
 
 
