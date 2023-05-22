e5746d4c124f93f44c158eb9b913fd5b451066cc
Arron2411616261 <15018135093@163.com>
2023-05-22 20:17:58 +0800

    切换地址-激活交互

--- frontend/src/views/Checkout/index.vue
+++ frontend/src/views/Checkout/index.vue
@@ -15,6 +15,17 @@ onMounted(() => getCheckInfo())
 // 控制弹框打开
 const showDialog = ref(false)
 
+// 切换地址
+const activeAddress = ref({})
+const switchAddress = (item) => {
+  activeAddress.value = item
+}
+const confirm = () => {
+  curAddress.value = activeAddress.value
+  showDialog.value = false
+  activeAddress.value = {}
+}
+
 </script>
 
 <template>
@@ -30,7 +41,7 @@ const showDialog = ref(false)
               <ul v-else>
                 <li><span>收<i />货<i />人：</span>{{ curAddress.receiver }}</li>
                 <li><span>联系方式：</span>{{ curAddress.contact }}</li>
-                <li><span>收货地址：</span>{{ curAddress.fullLocation }}{{ curAddress.address }}</li>
+                <li><span>收货地址：</span>{{ curAddress.fullLocation }} {{ curAddress.address }}</li>
               </ul>
             </div>
             <div class="action">
@@ -103,18 +114,19 @@ const showDialog = ref(false)
   <!-- 切换地址 -->
   <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
     <div class="addressWrapper">
-      <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+      <div class="text item" :class="{ active: activeAddress.id === item.id }" @click="switchAddress(item)"
+        v-for="item in checkInfo.userAddresses" :key="item.id">
         <ul>
           <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
           <li><span>联系方式：</span>{{ item.contact }}</li>
-          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation }} {{ item.address }}</li>
         </ul>
       </div>
     </div>
     <template #footer>
       <span class="dialog-footer">
         <el-button>取消</el-button>
-        <el-button type="primary">确定</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
       </span>
     </template>
   </el-dialog>
