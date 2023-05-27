0c6be9694c6b79b10f84a55b93a704a9f6e1afd7
13712266245 <531794253@qq.com>
2023-05-27 16:57:01 +0800

    token失效处理

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -2,6 +2,8 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/es/components/message/style/css'
+import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
   baseURL: 'https://mock.apifox.cn/m1/2726765-0-default',
@@ -15,11 +17,20 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  userStore = useUserStore()
+
   //统一提示错误
   ElMessage({
     type: 'warning',
     message: e.response.data.msg
   })
+
+  //token失效处理
+  if(e.response.status === 401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
+
   return Promise.reject(e)
 })
 
