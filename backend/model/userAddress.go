35d75b62073a4e739ee84f930ab88877ddba9e4d
winson02 <13712052798@163.com>
2023-06-10 16:51:13 +0800

    去掉了isdefault

--- backend/model/userAddress.go
+++ backend/model/userAddress.go
@@ -4,8 +4,7 @@ import "gorm.io/gorm"
 
 type UserAddress struct {
 	gorm.Model
-	Receiver  string `json:"receiver" gorm:"type:varchar(255);not null"`
-	Contact   string `json:"contact" gorm:"type:varchar(255);not null"`
-	Address   string `json:"address" gorm:"type:varchar(1024);not null"`
-	IsDefault bool   `json:"isDefault" gorm:"type:varchar(10);not null"`
+	Receiver string `json:"receiver" gorm:"type:varchar(255);not null"`
+	Contact  string `json:"contact" gorm:"type:varchar(255);not null"`
+	Address  string `json:"address" gorm:"type:varchar(1024);not null"`
 }
